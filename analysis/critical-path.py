b548a49e2ce7317db426afa4074a32d379f3bbd5
Kevin Huck <khuck@cs.uoregon.edu>
2025-04-01 15:14:14 -0700

    Updating script to correctly capture critical path

--- analysis/critical-path.py
+++ analysis/critical-path.py
@@ -19,7 +19,7 @@ parser.add_argument("--draw", dest="draw", action="store_true", default=False,
                     help="Draw critical-path graph as networkx graph")
 parser.add_argument("--verbose", dest="verbose", action="store_true", default=False,
                     help="Print critical-path nodes")
-parser.add_argument("--main-task", dest="main_task", default="1", required=False, type=int,
+parser.add_argument("--main-task", dest="main_task", default="0", required=False, type=int,
                     help="The main task GUID")
 parser.add_argument("--filename", dest="filename", default=None, required=False, type=str, help="The filename to parse (default is trace_events.*.json.gz)")
 
@@ -45,7 +45,10 @@ def critical_path_method(graph, root):
             est = max(est, graph.nodes[predecessor]["eft"])
         graph.nodes[node]["est"] = est
         graph.nodes[node]["eft"] = est + graph.nodes[node]["duration"]
-        endtime = max(endtime, (graph.nodes[node]["ts"] + graph.nodes[node]["duration"]))
+        tmpend = graph.nodes[node]["ts"] + graph.nodes[node]["duration"]
+        if tmpend > endtime:
+            #print(graph.nodes[node]["guid"],"ends at",tmpend)
+            endtime = tmpend
 
     # Backward pass
     for node in reversed(list(nx.topological_sort(graph))):
@@ -133,7 +136,7 @@ def readGraph(inargs):
     lastDestroy = 0
 
     for counter, infile in enumerate(sorted(glob.glob(myglob))):
-        print("Reading",infile)
+        print("\nReading",infile)
         #with open (infile, 'r') as jsonfile:
         if inargs.compressed:
             jsonfile = gzip.open(infile, "r")
@@ -142,6 +145,7 @@ def readGraph(inargs):
         data = json.load(jsonfile)
 
         for line in data["traceEvents"]:
+            """
             # look for this:
             # {"name":"APEX Trace Begin","ph":"R","pid":0,"tid":0,"ts":1739400926808546.250},
             if line["ph"] == "R" and line["name"] == "APEX Trace Begin":
@@ -156,10 +160,11 @@ def readGraph(inargs):
             if line["ph"] == "R" and line["name"] == "APEX Trace End":
                 end = max(end,line["ts"])
                 continue
+            """
 
             # look for this:
             # {"name":"Task Creation","ph":"R","pid":0,"tid":0,"ts":1741629788031981.250,"args":{"GUID":144678138029277185,"name":"APPLY_DIAG"}},
-            if firstCreate == 0 and line["ph"] == "R" and line["args"]["GUID"] > 1:
+            if firstCreate == 0 and line["ph"] == "R" and line["name"] == "Task Creation":
                 firstCreate = line["ts"]
                 continue
 
@@ -170,14 +175,21 @@ def readGraph(inargs):
                 continue
                 
             # look for this:
-            # {"name":"cudaGetDeviceCount","cat":"CPU","ph":"X","pid":0,"tid":0,"ts":1739400928607165.500,"dur":120.250,"args":{"GUID":2,"Parent GUID":1}},
+            # {"name":"cudaGetDeviceCount","cat":"CPU","ph":"X","pid":0,"tid":0,"ts":1739400928607165.500,"dur":120.250,"args":{"Implicit Parent":false,"GUID":2,"Parent GUID":1}},
             if line["ph"] == "X":
-                name = line["name"]
-                dur = line["dur"]
                 args = line["args"]
+                name = line["name"]
+                if "Implicit Parent" in args:
+                    if args["Implicit Parent"] and "apex_preload_main" not in name:
+                        continue
+                dur = line["dur"]
+                ts = line["ts"]
+                if "apex_preload_main" in name:
+                    duration = int(dur)
+                    begin = int(ts)
+                    dur = 0
                 guid = args["GUID"]
                 pguid = args["Parent GUID"]
-                ts = line["ts"]
                 # Set the "main" timer length to 0, so that it doesn't distort our analysis
                 #if (guid == inargs.main_task):
                     #dur = 0.0
@@ -191,12 +203,15 @@ def readGraph(inargs):
                         G.add_edge(pguid,guid)
 
         jsonfile.close()
+    if 0 in G.nodes and G.nodes[0] == {}:
+        G.add_node(0, guid=0, name="APEX MAIN", duration=0, args={"GUID":0,"Parent GUID":-1}, pguid=-1, ts=firstCreate)
 
-    duration = (end - begin) * 1.0e-6
+    #duration = (end - begin) * 1.0e-6
+    duration = duration * 1.0e-6
     path = (lastDestroy - firstCreate) * 1.0e-6
     print("Graph has",G.number_of_nodes(),"nodes and",G.size(),"edges.")
     print("Program took",duration,"seconds.")
-    print("Outermost critical path took",path,"seconds.")
+    print("Total tasks lifetime",path,"seconds.")
     return G
 
 def get_subgraph_from_node(graph, start_node):
@@ -233,6 +248,7 @@ def main():
                 break
 
     print("Critical path starting at node:", inargs.main_task)
+    print("    has",G.number_of_nodes(),"nodes and",G.size(),"edges.")
     if inargs.verbose:
         print("Critical path nodes:",critical_path)
     print("Critical path length:",len(critical_path))
